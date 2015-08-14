f9f63c43687187f91d7f3ab8e6b1b38474862885
yeinerarias <yeinerarias@gmail.com>
2015-08-14 15:33:53 -0600

    master continuos

--- alsa_master.c
+++ alsa_master.c
@@ -2,12 +2,25 @@
 #include <alsa/pcm.h>
 #include <math.h>
 #include <omp.h>
-#include <conio.h>
-#define BUFFER_LEN 44100//44100
+#include <termios.h>
+#include <stdio.h>
+#include <pthread.h>
 
+#define BUFFER_LEN 2048//44100
+
+static struct termios old, new;
+static struct termios g_old_kbd_mode;
 static char *device = "default";                       //soundcard
 snd_output_t *output = NULL;
 float buffer [BUFFER_LEN];
+float buffer_null [BUFFER_LEN];
+int send = 0;
+int quit_ctrl = 1;
+pthread_t tkc;
+int Fs = 44100;             //sampling frequency
+
+char getch(void);
+void *key_control();
 
 void gen_tones(int buffer_size, int digit, int Fs, float* input);
 
@@ -16,11 +29,13 @@ int main(void)
     int err;
     int j,k;
     
-    int Fs = 2048;             //sampling frequency
     int digit = 2;
 
     snd_pcm_t *handle;
-    snd_pcm_sframes_t frames;
+    
+    for(j = 0; j < BUFFER_LEN; j++) {
+		buffer_null[j] = 0;
+	}
 
 
     // ERROR HANDLING
@@ -36,19 +51,23 @@ int main(void)
                                   1,
                                   Fs,
                                   1,
-                                  500000)) < 0) {   
+                                  100000)) < 0) {   
             printf("Playback open error: %s\n", snd_strerror(err));
             exit(EXIT_FAILURE);
 
 
     }
-
-        gen_tones(BUFFER_LEN, digit, Fs, buffer);   
-
-        while(1){
-            frames = snd_pcm_writei(handle, buffer, BUFFER_LEN);    //sending values to sound driver
-            getch();
-            }
+ 
+	   pthread_create(&tkc, NULL, key_control, NULL);
+       while(quit_ctrl) {
+			if(send){
+				(void) snd_pcm_writei(handle, buffer, BUFFER_LEN);    //sending values to sound driver
+				send = 0;
+			}
+            else (void) snd_pcm_writei(handle, buffer_null, BUFFER_LEN);    //sending values to sound driver
+       }
+       //printf("Ñauuuuu  %c\n" , getch());
+            
 
     snd_pcm_close(handle);
     return 0;
@@ -117,3 +136,92 @@ void gen_tones(int buffer_size, int digit, int Fs, float* input)
 		input[i] = sin(2.0*M_PI*freq_down*i/Fs) + sin(2.0*M_PI*freq_up*i/Fs);
 	}
 }
+
+
+/* Initialize new terminal i/o settings */
+void initTermios(int echo) 
+{
+  tcgetattr(0, &old); /* grab old terminal i/o settings */
+  new = old; /* make new settings same as old settings */
+  new.c_lflag &= ~ICANON; /* disable buffered i/o */
+  new.c_lflag &= echo ? ECHO : ~ECHO; /* set echo mode */
+  tcsetattr(0, TCSANOW, &new); /* use these new terminal i/o settings now */
+}
+
+/* Restore old terminal i/o settings */
+void resetTermios(void) 
+{
+	tcsetattr(0, TCSANOW, &old);
+}
+
+/* Read 1 character - echo defines echo mode */
+char getch_(int echo) 
+{
+	char ch;
+	initTermios(echo);
+	ch = getchar();
+	resetTermios();
+	return ch;
+}
+
+char getch(void) 
+{
+	return getch_(0);
+}
+
+static void old_attr(void)
+{
+    tcsetattr(0, TCSANOW, &g_old_kbd_mode);
+}
+ 
+void *key_control() /*key control and reception thread*/
+    {
+    int hotkey;
+    static char init;
+    struct termios new_kbd_mode;
+ 
+    if(init)
+        return;
+       // put keyboard (stdin, actually) in raw, unbuffered mode
+    tcgetattr(0, &g_old_kbd_mode);
+    memcpy(&new_kbd_mode, &g_old_kbd_mode, sizeof(struct termios));
+       
+    new_kbd_mode.c_lflag &= ~(ICANON | ECHO);
+    new_kbd_mode.c_cc[VTIME] = 0;
+    new_kbd_mode.c_cc[VMIN] = 1;
+    tcsetattr(0, TCSANOW, &new_kbd_mode);
+    atexit(old_attr);
+     
+    while (quit_ctrl)
+    {
+        hotkey = getchar(); /*get char without interfere with main program*/
+ 
+        if (hotkey == (int) '1')
+        {
+			gen_tones(BUFFER_LEN, 1, Fs, buffer);
+			send = 1; 
+        }
+        else if (hotkey == (int) '2')
+        {
+			gen_tones(BUFFER_LEN, 2, Fs, buffer);
+			send = 1; 
+        }
+        else if (hotkey == (int) '3')
+        {
+			gen_tones(BUFFER_LEN, 3, Fs, buffer); 
+			send = 1;  
+        }
+        else if (hotkey == (int) '4')
+        {
+			gen_tones(BUFFER_LEN, 4, Fs, buffer);  
+			send = 1; 
+        }
+        else if (hotkey == (int) 'q' || hotkey == (int) 'Q')
+        {
+            quit_ctrl=0;
+        }
+        usleep(1000);   
+    }
+     pthread_exit(NULL);  
+}
+ 
