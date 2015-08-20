3f5e32416cf7d2dfd14843df0a6e9f008ed4a49a
Jeferson <jefg89@gmail.com>
2015-08-20 11:58:28 -0600

    led blink with export/unexport

--- hola_led.c
+++ hola_led.c
@@ -3,9 +3,13 @@
 
 
 int main() {
+
+system("echo 17 > /sys/class/gpio/export");
+system("echo out > /sys/class/gpio/gpio17/direction");
 system("echo 0 > /sys/class/gpio/gpio17/value");
 sleep(1);
 system("echo 1 > /sys/class/gpio/gpio17/value");
+system("echo 17 > /sys/class/gpio/unexport");
 return 0;
 }
 
