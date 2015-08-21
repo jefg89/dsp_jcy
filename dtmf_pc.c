49b355920a00f399da568d65d73c72778ada819e
Yeiner Arias <yeinerarias@gmail.com>
2015-08-21 11:22:36 -0600

    PC version

---
+++ dtmf_pc.c
@@ -0,0 +1,171 @@
+#include <menuGUI.h>
+#include <goertzel.h>
+
+static struct termios old, new;
+static struct termios g_old_kbd_mode;
+static char *device = "default";                       //soundcard
+float buffer_null[1024];
+
+int main(void)
+{
+	
+    int err;
+    int j,k;
+    num_det = 0;
+    
+    send = 0;
+    Fs = 44100;     //sampling frequency
+    durationdefaultsize = 0;
+    mode = 2;
+    num_jef[0] = 8;
+    num_jef[1] = 3;
+    num_jef[2] = 1;
+    num_jef[3] = 1;
+    num_jef[4] = 9;
+    num_jef[5] = 0;
+    num_jef[6] = 5;
+    num_jef[7] = 1;
+	digit = num_jef[0];
+
+/*	system("echo 17 > /sys/class/gpio/export");
+	system("echo out > /sys/class/gpio/gpio17/direction");
+	system("echo 1 > /sys/class/gpio/gpio17/value");*/
+    
+for(j = 0; j < 1024; j++) {
+		buffer_null[j] = 0;
+	}
+	buffer_2048 = (float **) malloc(sizeof(float*) * 16);
+	buffer_11793 = (float **) malloc(sizeof(float*) * 16);
+	buffer_22562 = (float **) malloc(sizeof(float*) * 16);
+	buffer_33331 = (float **) malloc(sizeof(float*) * 16);
+	buffer_44100 = (float **) malloc(sizeof(float*) * 16);
+
+	for(k=0; k<16;k++){
+		buffer_2048[k] = (float *) malloc(sizeof(float) * 2048 );
+		buffer_11793[k] = (float *) malloc(sizeof(float) * 11793 );
+		buffer_22562[k] = (float *) malloc(sizeof(float) * 22562 );
+		buffer_33331[k] = (float *) malloc(sizeof(float) * 33331 );
+		buffer_44100[k] = (float *) malloc(sizeof(float) * 44100 );
+	}
+	gen_tones(buffer_2048, 2048);
+	gen_tones(buffer_11793, 11793);
+	gen_tones(buffer_22562, 22562);
+	gen_tones(buffer_33331, 33331);
+	gen_tones(buffer_44100, 44100);
+	
+	set_station();
+	pthread_create( &thread1, NULL, finding_freq, NULL);
+	pthread_create(&tkc, NULL, menu, NULL);
+	while (ch1 != 'q') {
+		
+			if ((err = snd_pcm_open(&handle_w, device, SND_PCM_STREAM_PLAYBACK, 0)) < 0) {
+					printf("Playback open error: %s\n", snd_strerror(err));
+					exit(EXIT_FAILURE);
+			}
+
+			if ((err = snd_pcm_set_params(handle_w,
+										  SND_PCM_FORMAT_FLOAT,
+										  SND_PCM_ACCESS_RW_INTERLEAVED,
+										  1,
+										  Fs,
+										  1,
+										  100000)) < 0) {   
+					printf("Playback open error: %s\n", snd_strerror(err));
+					exit(EXIT_FAILURE);
+
+
+			}
+			while((mode==1) & (ch1 != 'q')) {
+				//printf("entramos del while enviar \n");
+				if(send){
+					switch( durationdefaultsize ) {
+						case 0:
+									(void) snd_pcm_writei(handle_w, buffer_2048[digit_], 2048);    //sending values to sound driver
+									break;
+						case 1:
+									(void) snd_pcm_writei(handle_w, buffer_11793[digit_], 11793);    //sending values to sound driver
+									break;
+						case 2:
+									(void) snd_pcm_writei(handle_w, buffer_22562[digit_], 22562);    //sending values to sound driver
+									break;
+						case 3:
+									(void) snd_pcm_writei(handle_w, buffer_33331[digit_], 33331);    //sending values to sound driver
+									break;
+						case 4:
+									(void) snd_pcm_writei(handle_w, buffer_44100[digit_], 44100);    //sending values to sound driver
+									break;
+						default:
+									break;
+					}
+				send = 0;
+				}
+				else (void) snd_pcm_writei(handle_w, buffer_null, 1024);    //sending values to sound driver
+			}
+			
+			snd_pcm_close(handle_w);
+			usleep(100000);
+			//sleep(1);
+		
+		
+			if ((err = snd_pcm_open (&handle_r, device, SND_PCM_STREAM_CAPTURE, 0)) < 0) {
+				exit (1);
+			}
+			if ((err = snd_pcm_set_params(handle_r,
+									  SND_PCM_FORMAT_FLOAT,
+									  SND_PCM_ACCESS_RW_INTERLEAVED,
+									  1,
+									  Fs,
+									  1,
+									  100000)) < 0) {   
+				printf("Playback open error: %s\n", snd_strerror(err));
+				exit(EXIT_FAILURE);
+					   }
+			
+			/*if ((err = snd_pcm_open(&handle_w, device, SND_PCM_STREAM_PLAYBACK, 0)) < 0) {
+					printf("Playback open error: %s\n", snd_strerror(err));
+					exit(EXIT_FAILURE);
+			}
+
+			if ((err = snd_pcm_set_params(handle_w,
+										  SND_PCM_FORMAT_FLOAT,
+										  SND_PCM_ACCESS_RW_INTERLEAVED,
+										  1,
+										  Fs,
+										  1,
+										  100000)) < 0) {   
+					printf("Playback open error: %s\n", snd_strerror(err));
+					exit(EXIT_FAILURE);
+
+
+			}*/
+			
+			
+			
+			//pthread_create( &thread2, NULL, write_, NULL);
+			digit = num_jef[0];
+			num_det = 0;
+			while((mode==0) & (ch1 != 'q')) { 
+				//printf("entramos del while recibido \n");
+				pthread_mutex_lock(&mutex_f);
+				pthread_mutex_unlock(&mutex_s);
+				pthread_mutex_lock(&mutex_w1);		
+				(void) snd_pcm_readi(handle_r, buffer_f, 2048); 
+				pthread_mutex_unlock(&mutex_w1);
+				pthread_mutex_lock(&mutex_s);
+				pthread_mutex_unlock(&mutex_f);
+				(void) snd_pcm_readi(handle_r, buffer_s, 2048);
+
+			}
+			snd_pcm_close(handle_r);
+			usleep(100000);
+			//snd_pcm_close(handle_w);
+			//sleep(1);
+			//printf("salimos del while recibido \n");
+		
+	}
+	
+	//system("echo 17 > /sys/class/gpio/unexport");
+
+    return 0;
+}
+ 
---
+++ goertzel_pc.c
@@ -0,0 +1,200 @@
+#include <goertzel.h>
+
+int g1, g2, g3, g4;
+int found = 1;
+
+float goertzel(int N,int Ft, float* input) {
+	
+    int     k, i;
+    float   floatN;
+    float   omega, sine, cosine, q0, q1, q2, power;
+
+    float   scalingFactor = N / 2.0;
+
+    floatN = (float) N;
+    k = (int) (0.5 + ((floatN * Ft) / Fs));
+    omega = (2.0 * M_PI * k) / floatN;
+    sine = sin(omega);
+    cosine = 2.0 *cos(omega);
+    q0=0;
+    q1=0;
+    q2=0;
+    for(i=0; i<N; i++)
+    {
+        q0 = cosine * q1 - q2 + input[i];
+        q2 = q1;
+        q1 = q0;
+    }
+
+    power = (q2 *q2 + q1 * q1 - cosine * q1 * q2) / scalingFactor;
+    return power;
+}
+
+void *finding_freq() {
+	double elapsed;
+	int err;
+	usleep(100000);
+	start = clock();
+	while(1) {
+		pthread_mutex_lock(&mutex_f);
+		#pragma omp parallel
+		{
+			g1 = goertzel(2048, freq_up, buffer_f);
+			g2 = goertzel(2048, freq_down, buffer_f);
+		}
+		if ((g1>TH) & 
+		   (g2>TH)){
+			if(found) {
+				//printf("Digito %f encontrado\n", end);
+				found = 0;
+				//system("echo 0 > /sys/class/gpio/gpio17/value");
+				dial_num[num_det] = num_jef[num_det];
+				num_det++;
+				digit = num_jef[num_det];
+				set_station();
+				start = clock();
+			}
+		}
+		else {
+			found = 1;
+			//system("echo 1 > /sys/class/gpio/gpio17/value");
+		}
+		pthread_mutex_unlock(&mutex_f);
+		pthread_mutex_lock(&mutex_s);
+		#pragma omp parallel
+		{
+			g3 = goertzel(2048, freq_up, buffer_s);
+			g4 = goertzel(2048, freq_down, buffer_s);
+		}
+		if ((g3>TH) & 
+		   (g4>TH)){
+			if(found) {
+				//printf("Digito %f encontrado\n", end);
+				found = 0;
+				//system("echo 0 > /sys/class/gpio/gpio17/value");
+				dial_num[num_det] = num_jef[num_det];
+				num_det++;
+				digit = num_jef[num_det];
+				set_station();
+				start = clock();
+			}
+		}
+		else {
+			found = 1;
+			//system("echo 1 > /sys/class/gpio/gpio17/value");
+		}
+		pthread_mutex_unlock(&mutex_s);
+		end = clock();
+		elapsed = ((double) (end - start)) / CLOCKS_PER_SEC;
+		if(abs(elapsed) > 3) {
+			digit = num_jef[0];
+			num_det = 0;
+			set_station();
+			//printf("Digito %f encontrado\n", digit);
+			
+		}
+		if(num_det == 8) {
+			if ((err = snd_pcm_open(&handle_w, "default", SND_PCM_STREAM_PLAYBACK, 0)) < 0) {
+					printf("Playback open error: %s\n", snd_strerror(err));
+					exit(EXIT_FAILURE);
+			}
+
+			if ((err = snd_pcm_set_params(handle_w,
+										  SND_PCM_FORMAT_FLOAT,
+										  SND_PCM_ACCESS_RW_INTERLEAVED,
+										  1,
+										  Fs,
+										  1,
+										  100000)) < 0) {   
+					printf("Playback open error: %s\n", snd_strerror(err));
+					exit(EXIT_FAILURE);
+
+
+			}
+			pthread_create( &thread2, NULL, write_, NULL);
+			num_det = 0;
+		}
+	}
+}
+
+
+void *write_() {
+	usleep(100000);
+	while((mode==0) & (ch1 != 'q'))
+	{	
+		//system("echo 0 > /sys/class/gpio/gpio17/value");
+		pthread_mutex_lock(&mutex_w1);
+		(void) snd_pcm_writei(handle_w, buffer_f, 2048);
+		pthread_mutex_unlock(&mutex_w1); 
+		(void) snd_pcm_writei(handle_w, buffer_s, 2048);
+
+	}
+	snd_pcm_close(handle_w);
+	//system("echo 1 > /sys/class/gpio/gpio17/value");
+	usleep(100000);
+}
+
+void set_station() {
+	switch ( digit ) {
+        case 0:        
+            freq_up = 1336; freq_down = 941;  
+            break;
+        case 1:
+			freq_up = 1209; freq_down = 697;         
+            break;
+        case 2:          
+            freq_up = 1336; freq_down = 697;
+            break;
+        case 3:         
+            freq_up = 1477; freq_down = 697;
+            break;
+        case 4:        
+            freq_up = 1209; freq_down = 770;
+            break;
+        case 5:        
+            freq_up = 1336; freq_down = 770;
+            break;
+        case 6:        
+            freq_up = 1477; freq_down = 770;
+            break;
+        case 7:        
+            freq_up = 1209; freq_down = 852;
+            break;
+        case 8:        
+            freq_up = 1336; freq_down = 852;
+            break;
+        case 9:        
+            freq_up = 1477; freq_down = 852;
+            break;
+        case 10:   /* A */ 
+            freq_up = 1633; freq_down = 697;
+            break;
+        case 11:   /* B */     
+            freq_up = 1633; freq_down = 770;
+            break;
+        case 12:   /* C */     
+            freq_up = 1633; freq_down = 852;
+            break; 
+        case 13:   /* D */    
+            freq_up = 1333; freq_down = 941;
+            break;
+        case 14:   /* * */    
+            freq_up = 1209; freq_down = 941;
+            break;
+        case 15:   /* # */      
+            freq_up = 1477; freq_down = 941;
+            break;
+        default:   /*Any*/       
+            freq_up = 10000; freq_down = 10000;
+            break;
+	}
+}
+unsigned long get_time_usec() {
+	
+	struct timeval tv;
+	gettimeofday(&tv,NULL);
+	unsigned long time_in_micros = 1000000 * tv.tv_sec + tv.tv_usec;
+	return time_in_micros;
+}
+
+
